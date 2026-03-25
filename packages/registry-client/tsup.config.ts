3f24198d34b0d31e9899625e1122d4e45750f52c
Todd Palmer <todd@betterdata.co>
2026-03-25 09:20:48 -0700

    fix(registry-client): remove orphaned betterdata entry point

--- packages/registry-client/tsup.config.ts
+++ packages/registry-client/tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: {
     index: "src/index.ts",
-    betterdata: "betterdata.ts"
   },
   format: ["cjs", "esm"],
   dts: true,
